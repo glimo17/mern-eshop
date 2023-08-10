ccd4a966ab05cb1ecd599c96135c4c8269aaa3e7
Jeank <glimo17@gmail.com>
2023-08-09 21:10:29 -0600

    nuevo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -170,7 +170,7 @@ function App() {
               <strong>Categories</strong>
             </Nav.Item>
 
-            {categories.map((category) => (
+            {/* {categories.map((category) => (
               <Nav.Item key={category}>
                 <LinkContainer
                   to={`/search?category=${category}`}
@@ -179,7 +179,7 @@ function App() {
                   <Nav.Link>{category}</Nav.Link>
                 </LinkContainer>
               </Nav.Item>
-            ))}
+            ))} */}
           </Nav>
         </div>
         <main>
